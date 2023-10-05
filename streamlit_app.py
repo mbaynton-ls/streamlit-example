d111dc93beb606bef833b0b6979af97c20a2e3f8
mbaynton-ls <147015974+mbaynton-ls@users.noreply.github.com>
2023-10-05 13:48:34 -0500

    modify num_turns in code

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,8 @@ with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
+    num_turns = num_turns * 2
+
     Point = namedtuple('Point', 'x y')
     data = []
 
